5ac65b8648484b70ba069815d4a498a300079e77
Alex UD <alex.ud@webhive.com.ua>
2025-03-28 14:03:14 +0200

    added riverpod package

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:my_spotify_app/views/home/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ProviderScope(child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
